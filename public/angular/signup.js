4fb988e0533f4d1bd03af85120bfcb9046bb69fb
ganesh710 <ganeshrgppc.710@gmail.com>
2020-08-20 17:03:31 +0530

    commited

--- public/angular/signup.js
+++ public/angular/signup.js
@@ -6,7 +6,12 @@ app.controller("MYCONTROLLER",function($scope,$http) {
 			method:'post',
 			url:'/postsignupdata',
 			data:$scope.user
-		})
+		}).then(function success(responce) {
+			alert("user added succesfully");
+			$scope.user = {};
+		}),function error(responce) {
+			alert("error occured!please try again later");
+		}
 
 	}
 	
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var monk = require('monk');
+var db = monk('localhost:27017/example');
+var collection = db.get('signupdata');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +11,16 @@ router.get('/', function(req, res, next) {
 });
 router.post('/postsignupdata', function(req, res) {
   console.log(req.body);
+  collection.insert(req.body,function(err,docs) {
+  	if(err){
+  		console.log("error");
+  	}
+  	else{
+  		console.log("details sent to database");
+  		console.log(docs);
+  		res.send(docs);
+  	}
+  });
 });
 
 module.exports = router;
